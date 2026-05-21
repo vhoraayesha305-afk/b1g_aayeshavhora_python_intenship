ab1ce23d83fada614fd6bc81e08d6b7f81db72b3
vhoraayesha305-afk <vhoraayesha305@gmail.com>
2026-05-22 02:00:05 +0530

    day 7 complet

---
+++ day7/task1_print_numbers.py
@@ -0,0 +1,4 @@
+for i in range(1, 200):
+    print(i, end=" ")
+
+print()  # This moves the cursor to a new line at the very end
---
+++ day7/task2_multiplication_table.py
@@ -0,0 +1,6 @@
+num =int(input("enter no:"))
+
+for i in range(1,11):
+    print(f"{num} x {i} = {num * i}")
+    
+
